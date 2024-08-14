8fa73b174e0c17d9755710ba7fc8ee176fe70912
Shruti-Surjuse <114313250+Shruti-Surjuse@users.noreply.github.com>
2024-08-14 17:10:29 +0000

    Conversion of datatype

---
+++ 01_basics/datatypeConversion.js
@@ -0,0 +1,77 @@
+// let marks=33
+// console.log(typeof marks);
+// console.log(typeof(marks)); // its a methode can write like this as well
+
+// let mark="33"
+// console.log(typeof mark);// its a string
+// let valueInNum=Number(mark)// it will convert it into num
+// console.log(valueInNum);
+
+
+// let a="33abss" // it will return string but it's not a num nor string it's a NaN value (not a number)
+// console.log(typeof a);//string
+// console.log(a);
+// let value=Number(a)
+// console.log(value);
+
+
+// let b=null // it will return the type of b as object , value number 
+// console.log(b);
+// console.log(typeof b);
+
+// let value=Number(b)
+// console.log(value);
+// console.log(typeof value);
+
+
+// let b=undefined// it will return the type of b undefined and after converting it , will return the  number as a type 
+// console.log(b);
+// console.log(typeof b);
+
+// let value=Number(b)
+// console.log(value);
+// console.log(typeof value);
+
+// let b=true // it will return the type of b boolean , value 1 
+// console.log(b);
+// console.log(typeof b);
+
+// let value=Number(b)
+// console.log(value);
+// console.log(typeof value);
+
+// let n="Shruti"
+// console.log(n);
+// console.log(typeof n);
+
+// let value=Number(n);
+// console.log(value);
+// console.log(typeof value);
+
+// let a=0;
+// console.log(a);
+// console.log(typeof a);
+
+// let value=Boolean(a);
+// console.log(value);
+// console.log(typeof value);
+
+// a=""=> false, a="shruti"=>true,a=0=>false
+
+let b=33
+console.log(b);
+console.log(typeof b);
+
+let string=String(b)
+console.log(string);
+console.log(typeof string);
+
+
+
+
+
+
+
+
+
+
