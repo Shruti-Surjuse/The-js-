8f3d8849739e9b347b3a747a563e164147e36d47
Shruti-Surjuse <114313250+Shruti-Surjuse@users.noreply.github.com>
2024-09-08 12:46:29 +0000

    strings

---
+++ 01_basics/string.js
@@ -0,0 +1,41 @@
+//In strings there are lot of functions to see that go on console and using prototype we can see this 
+// const name="shry"// 1st way 
+// const value=7
+// console.log(name+value);
+
+// const name1=new String("shrui")//2nd way 
+
+// // console.log(`My ${name1} and value is ${value}`); //modern way 
+// console.log(name1.__proto__);// it will give the empty obj 
+// console.log(name1.length);
+// // console.log(name1.toUppercase());
+// console.log(name1.charAt(1));
+// console.log(name1.indexOf('t'));
+
+// const sub=new String("shruti") 
+// console.log(sub.substring(0,3));// in substring methid it doesn't inclue last index value , in we can not give the negative value
+
+// const slic=new String("shruti")
+// console.log(slic.slice(3,5));  // in slice we can give the negative index in this it will give the value from end 
+
+// const repl=new String("shruti")
+// console.log(repl.replace("i","u"));
+
+// const tri=new String("shruti        ")
+// console.log(tri.trim());
+// console.log(tri.includes("shh"));// we can ask this and it will provide the answer in boolean 
+
+const spl=new String("Shriti Shuru Surjuse")
+console.log(spl.split(" "));// it will provide the answer in the form of array and it take two arguments sepratore and the limit in this I only give the seprator 
+
+
+
+
+
+
+ 
+
+
+
+
+
